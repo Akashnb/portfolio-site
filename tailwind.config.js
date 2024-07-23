6897273fe08ccf47ced1f11e56c58f0caea8ea88
akash <akashnb97@gmail.com>
2024-07-23 21:52:23 +0530

    CSS fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        wave: "wave-animation 2.5s infinite",
+      },
+      keyframes: {
+        "wave-animation": {
+          "0%": { transform: "rotate(0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10deg)" },
+          "60%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+      },
       backgroundImage: {
         "custom-gradient":
           "linear-gradient(90deg, #4AB1F1 0%, #566CEC 35.5%, #D749AF 71.5%, #FF7C51 94.5%)",
